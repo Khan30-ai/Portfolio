b7117770639368b7dd4b8a15d93c9476d4b97ed4
Md. Arshi Khan <arshikhan80708@gmail.com>
2026-06-28 22:39:00 +0530

    corrected spellings

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navbar from './components/sections/Navbar';
-import HeroSection from './components/sections/Hero';
-import AboutSection from './components/sections/About';
-import TechStackSection from './components/sections/TechStack';
-import EducationSection from './components/sections/Education';
+import Hero from './components/sections/Hero';
+import About from './components/sections/About';
+import TechStack from './components/sections/TechStack';
+import Education from './components/sections/Education';
 import ProjectsSection from './components/sections/ProjectSection';
-import WritingsSection from './components/sections/Writing';
+import Writing from './components/sections/Writing';
 import ContactFooter from './components/sections/ContactFooter';
 
 export default function Home() {
@@ -13,12 +13,12 @@ export default function Home() {
         <div className="bg-[#09090B] text-zinc-50 min-h-screen selection:bg-emerald-500/30 selection:text-emerald-400">
             <Navbar />
             <main className="pt-16">
-                <HeroSection />
-                <AboutSection />
-                <TechStackSection />
-                <EducationSection />
+                <Hero />
+                <About />
+                <TechStack />
+                <Education />
                 <ProjectsSection />
-                <WritingsSection />
+                <Writing />
                 <ContactFooter />
             </main>
         </div>
